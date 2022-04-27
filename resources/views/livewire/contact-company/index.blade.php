61972ce33d4bc224f09c4c17f7eb2239649ebeb5
chicaiyaochibaicaixin <xiangbing@88.com>
2022-04-27 14:28:37 +0800

    update

--- resources/views/livewire/contact-company/index.blade.php
+++ resources/views/livewire/contact-company/index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="card-controls sm:flex">
         <div class="w-full sm:w-1/2">
-            Per page:
+            分页:
             <select wire:model="perPage" class="form-select w-full sm:w-1/6">
                 @foreach($paginationOptions as $value)
                     <option value="{{ $value }}">{{ $value }}</option>
@@ -9,12 +9,12 @@
             </select>
 
             <button class="btn btn-rose ml-3 disabled:opacity-50 disabled:cursor-not-allowed" type="button" wire:click="confirm('deleteSelected')" wire:loading.attr="disabled" {{ $this->selectedCount ? '' : 'disabled' }}>
-                {{ __('Delete Selected') }}
+                删除
             </button>
 
         </div>
         <div class="w-full sm:w-1/2 sm:text-right">
-            Search:
+            搜索:
             <input type="text" wire:model.debounce.300ms="search" class="w-full sm:w-1/3 inline-block" />
         </div>
     </div>
